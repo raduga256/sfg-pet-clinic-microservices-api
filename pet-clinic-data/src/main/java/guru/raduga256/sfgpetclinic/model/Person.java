d1eb29ecf688f56097367507d05b62945978f44d
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-04-21 01:12:07 +0300

    Adding Id to model objects by extending BaseEntity accordingly. closes #10

--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package guru.raduga256.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private  String firstName;
     private  String lastName;
 
--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package guru.raduga256.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
