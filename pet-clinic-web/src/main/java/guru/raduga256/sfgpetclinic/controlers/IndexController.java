2bd699acd2b696f5eb618ff8a5cc4e515913f63e
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-04-21 13:29:20 +0300

    Adding Index page and Controller. closes #6

---
+++ pet-clinic-web/src/main/java/guru/raduga256/sfgpetclinic/controlers/IndexController.java
@@ -0,0 +1,14 @@
+package guru.raduga256.sfgpetclinic.controlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "index", "index.html"})
+    public String index(){
+
+        return "index";
+    }
+}
