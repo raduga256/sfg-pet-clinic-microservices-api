03c96d957485c254560f1d5d1bf14ce68298a13f
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-04-21 04:16:29 +0300

    Refactoring to common crud interface. closes #11

--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Vet.java
@@ -1,4 +1,4 @@
 package guru.raduga256.sfgpetclinic.model;
 
-public class Vet extends Person {
+public class  Vet extends Person {
 }
--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/services/PetService.java
@@ -2,13 +2,7 @@ package guru.raduga256.sfgpetclinic.services;
 
 import guru.raduga256.sfgpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
 
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
