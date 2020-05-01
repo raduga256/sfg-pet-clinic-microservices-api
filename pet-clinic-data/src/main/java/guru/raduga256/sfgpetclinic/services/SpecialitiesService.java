dbbae8b10875845fa0fb6ba7186b96cc979b8298
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-05-01 17:58:50 +0300

    Added a Specialities Map implementation. closes #31

---
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,7 @@
+package guru.raduga256.sfgpetclinic.services;
+
+import guru.raduga256.sfgpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/services/map/SpecialitiesMapService.java
@@ -0,0 +1,35 @@
+package guru.raduga256.sfgpetclinic.services.map;
+
+import guru.raduga256.sfgpetclinic.model.Speciality;
+import guru.raduga256.sfgpetclinic.services.SpecialitiesService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+@Service
+public class SpecialitiesMapService extends AbstractMapService<Speciality, Long> implements SpecialitiesService {
+
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
