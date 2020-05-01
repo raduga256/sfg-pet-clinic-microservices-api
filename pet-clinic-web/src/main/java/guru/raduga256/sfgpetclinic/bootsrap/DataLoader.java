4fa3b7e5ecf2cd2e52e7ffc73d7ffc237f4c3038
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-05-01 15:38:43 +0300

    Added PetTypes with Bootsrap data. closes #32

--- pet-clinic-web/src/main/java/guru/raduga256/sfgpetclinic/bootsrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/raduga256/sfgpetclinic/bootsrap/DataLoader.java
@@ -1,8 +1,10 @@
 package guru.raduga256.sfgpetclinic.bootsrap;
 
 import guru.raduga256.sfgpetclinic.model.Owner;
+import guru.raduga256.sfgpetclinic.model.PetType;
 import guru.raduga256.sfgpetclinic.model.Vet;
 import guru.raduga256.sfgpetclinic.services.OwnerService;
+import guru.raduga256.sfgpetclinic.services.PetTypeService;
 import guru.raduga256.sfgpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType savedCatPetType = petTypeService.save(cat);
+
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
