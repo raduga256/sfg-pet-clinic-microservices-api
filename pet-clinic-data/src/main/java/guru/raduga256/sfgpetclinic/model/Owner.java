b422a602c0768591ef35f6f562cdd01f055ddffe
raduga256 <56635875+raduga256@users.noreply.github.com>
2020-05-04 00:07:27 +0300

    Updating Owners to an Entity with Mappings. closes #36

--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/Owner.java
@@ -1,14 +1,23 @@
 package guru.raduga256.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
 
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owners")
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
--- pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/raduga256/sfgpetclinic/model/PetType.java
@@ -1,7 +1,16 @@
 package guru.raduga256.sfgpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity {
+
+    @Column(name = "name")
     private String name;
+
 
     public String getName() {
         return name;
